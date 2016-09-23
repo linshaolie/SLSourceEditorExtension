82dfa048294229ef708852ed1e31abbce46d7cef
linshaolie <lshaolie@163.com>
2016-09-24 01:50:53 +0800

    Initial Commit

---
+++ SLCommand/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SLCommand
+//
+//  Created by shaolie on 16/9/24.
+//  Copyright © 2016年 shaolie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
